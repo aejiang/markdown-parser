80c192fbca8e71983fa18e02ddce5be3789762ce
aerny-jiang <aernyjiang@gmail.com>
2022-04-21 10:43:39 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+
+public class MarkdownParseTest {  // class for tests 
+    @Test  // annotattion for JUnit to know this is a test case 
+    public void addition() {  // test method header 
+        assertEquals(2, 1 + 1);  // checks actual value 1 + 1 == expected 2
     }
 }
